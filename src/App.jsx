f9a914c7463b8594c12c5e7967b56857c6d818f8
wenddii <wendwosen03@gmail.com>
2026-06-15 20:22:46 +0300

    updated my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Certificates from "./components/Certificates";
 import Extracurriculars from "./components/Extracurriculars";
@@ -9,15 +12,19 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div>
+    <div className="app-shell">
       <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Certificates /> 
-      <Extracurriculars />
-      <Resume/>
-      <Contact />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Certificates />
+        <Extracurriculars />
+        <Resume />
+        <Contact />
+      </main>
     </div>
   );
 }
